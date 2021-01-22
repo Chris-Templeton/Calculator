c00ca856eb463f94e76dcb28abb7680aeddc4b01
Chris Templeton <christemp188@gmail.com>
2021-01-22 13:52:26 -0500

    adding command-line calculator project: currently can do +-*/^ and basic error checking

---
+++ CommandLineCalc/CommandLineCalc/Calculator.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace CommandLineCalc
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            // introduction to app
+            WriteIntro();
+
+            // allows multiple calculations to be made without restarting the program
+            bool running = true;
+            while (running)
+            {
+                // get user input
+                string input = GetUserInput();
+
+                if (input == "q")
+                {
+                    running = false;
+                }
+
+                if (running)
+                {
+                    string result;
+                    try
+                    {
+                        // parse user input
+                        IMathTree tree = ParseUserInput(input);
+
+                        // calculate result
+                        result = $"Equals: {tree.Solve()}";
+                    }
+                    catch (FormatException)
+                    {
+                        result = "Formatting Error";
+                    }
+
+                    // display result
+                    Console.WriteLine(result);
+                }
+            }
+        }
+
+        /* Writes formatted into text to console.
+         * 
+         * No arg(s).
+         * 
+         * No return.
+         */
+        static void WriteIntro()
+        {
+            Console.WriteLine("#################################");
+            Console.WriteLine("#### Command Line Calculator ####");
+            Console.WriteLine("####           V1            ####");
+            Console.WriteLine("####     Chris Templeton     ####");
+            Console.WriteLine("#################################");
+            Console.WriteLine();
+            Console.WriteLine("           'q' to quit           ");
+        }
+
+        /* Prompts user for input
+         * 
+         * No arg(s)
+         * 
+         * Return: user input
+         */
+        static string GetUserInput()
+        {
+            Console.Write("\nEnter Expression: ");
+            string input = Console.ReadLine();
+
+            // if string starts with a '-' (ie -7) add a 0 to the front to force subtraction
+            if (!String.IsNullOrEmpty(input) && input[0] == '-')
+            {
+                input = $"0{input}";
+            }
+
+            return input;
+        }
+
+        /* Parses input of user into something we can solve
+         * 
+         * input: user input
+         * 
+         * Return: IMathTree with values 'branching' to solve
+         *  
+         */
+        static IMathTree ParseUserInput(string input)
+        {
+            foreach (char op in IMathTree.Chars)
+            {
+                if (op == ' ') // base case, accounts for no operators left in string
+                {
+                    return (IMathTree)Activator.CreateInstance(IMathTree.Types[op], double.Parse(input));
+                }
+                else if (input.Contains(op))
+                {
+                    return (IMathTree)Activator.CreateInstance(IMathTree.Types[op], SplitTree(input, op));
+                }
+            }
+
+            throw new FormatException(); // this should never be reached
+        }
+
+        /* Helper method to ParseUserInput
+         * 
+         * Takes an input string and a operator (char)
+         * 
+         * Returns a IMathTree array split by that operator
+         */
+        static IMathTree[] SplitTree(string input, char op)
+        {
+            string[] split = input.Split(op);
+            IMathTree[] output = new IMathTree[split.Length];
+            for (int i = 0; i < split.Length; i++)
+            {
+                output[i] = ParseUserInput(split[i]);
+            }
+            return output;
+        }
+    }
+}
---
+++ CommandLineCalc/CommandLineCalc/MathTree.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandLineCalc
+{
+    interface IMathTree
+    {
+        // Chars must be the same as Types.Keys but ordered by reverse mathematical order of ops
+        public static List<char> Chars = new List<char> { '+', '-', '*', '/', '^', ' ' };
+        
+        // public dictionary of types of IMathTree items
+        public static Dictionary<char, Type> Types = new Dictionary<char, Type>()
+        {
+            {'+', typeof(Addition) },
+            {'-', typeof(Subtraction) },
+            {'*', typeof(Multiplication) },
+            {'/', typeof(Division) },
+            {'^', typeof(Exponent) },
+            {' ', typeof(Base) }
+        };
+
+        double Solve();
+    }
+
+    class Addition : IMathTree
+    {
+        private IMathTree[] Values;
+
+        public Addition(params IMathTree[] values)
+        {
+            Values = values;
+        }
+
+        public double Solve()
+        {
+            double result = 0;
+            foreach (IMathTree value in Values)
+            {
+                result += value.Solve();
+            }
+            return result;
+        }
+    }
+
+    class Subtraction : IMathTree
+    {
+        private IMathTree[] Values;
+
+        public Subtraction(params IMathTree[] values)
+        {
+            Values = values;
+        }
+
+        public double Solve()
+        {
+            double result = Values[0].Solve();
+            for (int i = 1; i < Values.Length; i++)
+            {
+                result -= Values[i].Solve();
+            }
+            return result;
+        }
+    }
+
+    class Multiplication : IMathTree
+    {
+        private IMathTree[] Values;
+
+        public Multiplication(params IMathTree[] values)
+        {
+            Values = values;
+        }
+
+        public double Solve()
+        {
+            double result = Values[0].Solve();
+            for (int i = 1; i < Values.Length; i++)
+            {
+                result *= Values[i].Solve();
+            }
+            return result;
+        }
+    }
+
+    class Division : IMathTree
+    {
+        private IMathTree[] Values;
+
+        public Division(params IMathTree[] values)
+        {
+            Values = values;
+        }
+
+        public double Solve()
+        {
+            double result = Values[0].Solve();
+            for (int i = 1; i < Values.Length; i++)
+            {
+                result /= Values[i].Solve();
+            }
+            return result;
+        }
+    }
+
+    class Exponent : IMathTree
+    {
+        private IMathTree[] Values;
+
+        public Exponent(params IMathTree[] values)
+        {
+            Values = values;
+        }
+
+        public double Solve()
+        {
+            double result = Values[0].Solve();
+            for (int i = 1; i < Values.Length; i++)
+            {
+                result = Math.Pow(result, Values[i].Solve());
+            }
+            return result;
+        }
+    }
+
+    class Base : IMathTree
+    {
+        private double Value;
+        public Base(double value)
+        {
+            Value = value;
+        }
+
+        public double Solve()
+        {
+            return Value;
+        }
+    }
+}
